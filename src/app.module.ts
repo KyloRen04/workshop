31eb66724581c47ec9102afd0a461a4299dbf1aa
Akshat Kumar <kumar.akshat29@gmail.com>
2022-11-06 23:26:27 +0530

    Connected to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://Akshat:I3eakCjkzhclBkJ1@cluster0.82e6z.mongodb.net/?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
